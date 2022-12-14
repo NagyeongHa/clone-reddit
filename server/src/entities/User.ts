863b0c78796da904ad1446ea59498368ff02fde3
NagyeongHa <hnk3176@naver.com>
2022-12-15 01:30:03 +0900

    User,Post,Sub Entity 작성

--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -1,18 +1,42 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { IsEmail } from "class-validator";
+import { Length } from "class-validator/types/decorator/decorators";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Index,
+  OneToMany,
+  BeforeInsert,
+} from "typeorm";
+import bcrypt from "bcryptjs";
+import { Exclude } from "class-transformer";
+import BaseEntity from "./Entity";
+@Entity("users")
+export default class User extends BaseEntity {
+  @Index()
+  @IsEmail(undefined, { message: "이메일 주소가 잘못되었습니다." })
+  @Length(1, 255, { message: "이메일 주소는 비워둘 수 없습니다." })
+  @Column({ unique: true })
+  email: string;
 
-@Entity()
-export class User {
+  @Index()
+  @Length(3, 32, { message: "사용자 이름은 3자 이상이어야 합니다." })
+  @Column({ unique: true })
+  username: string;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Exclude()
+  @Column()
+  @Length(6, 255, { message: "비밀번호는 6자리 이상이어야 합니다." })
+  password: string;
 
-    @Column()
-    firstName: string
+  @OneToMany(() => Post, post => post.user)
+  posts: Post[];
 
-    @Column()
-    lastName: string
+  @OneToMany(() => Vote, vote => vote.user)
+  votes: Vote[];
 
-    @Column()
-    age: number
-
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6);
+  }
 }
